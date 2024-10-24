005958b1ea10dfc0d6979ac158fb867348af07da
Raad05 <yamin.raad6109@gmail.com>
2024-10-24 15:33:17 +0600

    created smart contract boilerplate

--- src/icp_auction_dApp_backend/src/lib.rs
+++ src/icp_auction_dApp_backend/src/lib.rs
@@ -1,4 +1,57 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use candid::{CandidType, Decode, Deserialize, Encode};
+use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
+use ic_stable_structures::{BoundedStorable, DefaultMemoryImpl, StableBTreeMap, Storable};
+use std::{borrow::Cow, cell::RefCell};
+
+type Memory = VirtualMemory<DefaultMemoryImpl>;
+
+const MAX_VALUE_SIZE: u32 = 100;
+
+// enums
+#[derive(CandidType, Deserialize)]
+enum BidError {
+    AlreadyBid,
+    NoSuchItem,
+    AccessRejected,
+    UpdateError,
+}
+
+// structs
+#[derive(CandidType, Deserialize)]
+struct Item {
+    name: String,
+    description: String,
+    is_listed: bool,
+    bid_users: Vec<candid::Principal>,
+    owner: candid::Principal,
+}
+
+#[derive(CandidType, Deserialize)]
+struct CreateItem {
+    name: String,
+    description: String,
+    is_listed: bool,
+}
+
+// implementations
+impl Storable for Item {
+    fn to_bytes(&self) -> Cow<[u8]> {
+        Cow::Owned(Encode!(self).unwrap())
+    }
+
+    fn from_bytes(bytes: Cow<[u8]>) -> Self {
+        Decode!(bytes.as_ref(), Self).unwrap()
+    }
+}
+
+impl BoundedStorable for Item {
+    const MAX_SIZE: u32 = MAX_VALUE_SIZE;
+    const IS_FIXED_SIZE: bool = false;
+}
+
+// thread_local
+thread_local! {
+    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> = RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
+
+    static ITEM_MAP:RefCell<StableBTreeMap<u64, Item, Memory>> = RefCell::new(StableBTreeMap::init(MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0)))));
 }
